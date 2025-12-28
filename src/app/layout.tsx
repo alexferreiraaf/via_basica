9c8635c7e778a0125517740fc0d9389c073071fb
Alex Ferreira <alexescolpi@gmail.com>
2025-12-28 07:17:35 +0000

    Quando eu baixar o aplicativo no celular ou computador deve vir com o no

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Header } from '@/components/header';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
 
-const APP_NAME = "Livraria Via Básica";
+const APP_NAME = "Via Básica";
 const APP_DESCRIPTION = "Literatura que edifica sua vida.";
 
 export const metadata: Metadata = {
